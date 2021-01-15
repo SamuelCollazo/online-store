ad7cbf6d19d580b2e175c930902d4dfd208fc97b
SamuelCollazo <bananachockoloco99@gmail.com>
2021-01-15 19:48:30 +0100

    This is a new commit for what I originally planned to be amended

---
+++ src/App.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <header>
+      <h1 class ="siteName">Paraknas</h1>
+      <button v-on:click="navigateTo('Products')">View Products</button>
+      {{cart.length}}
+      <button v-on:click="navigateTo('Cart')">View Cart</button>
+    </header>
+
+    <div v-if="page === 'Cart'">
+      <Cart v-on:RemoveItemFromCart="RemoveItemFromCart" :cart="cart"/>
+    </div>
+
+    <div v-if="page === 'Products'">
+      <Products v-on:AddItemToCart="AddItemToCart"/>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Products from "./components/Products.vue"
+import Cart from "./components/Cart.vue"
+
+
+export default {
+  name: 'App',
+  data: () => {
+    return {
+      page: 'Products',
+      cart: [],
+
+    }
+  },
+
+  methods: {
+    AddItemToCart(product) {
+      this.cart.push(product);
+    },
+    RemoveItemFromCart(product) {
+      this.cart.splice(this.cart.indexOf(product), 1);
+    },
+    navigateTo(page) {
+      this.page = page;
+    },
+    
+
+  },
+  components: {
+    Products,
+    Cart,
+
+
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+.ProductsWrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+
+button {
+  cursor: pointer;
+}
+</style>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 0%;
+  padding: 0%;
+}
+
+header {
+  position: relative;
+  height: 120px;
+  background-color: lightblue;
+  box-shadow: 2px 2px 5px #999;
+  text-align:right;
+  font-size: 20px;
+  padding-top: 20px;
+  bottom: 60px;
+}
+
+
+
+.siteName {
+  position: relative;
+  right:542px;
+  margin-bottom: 0;
+  top: 30px;
+}
+
+
+
+
+
+
+</style>
---
+++ src/components/Products.vue
@@ -0,0 +1,151 @@
+<template>
+    <div>
+  <h1>Products</h1>
+      <div class="ProductsWrapper">
+        <div class ="card" v-for='product  in products' 
+          :key='product'>
+          <h2>{{product.name}}</h2>
+          <carousel :items-to-show="1" :wrap-around="true">
+            <slide v-for="image in product.images" 
+              :key="image">
+              <img :src="image" alt="picture should be here" >    
+            </slide>
+            <template #addons>
+              <navigation />
+              <pagination />
+            </template>
+          </carousel>
+          <div>{{product.cost}}</div>
+          <button v-on:click="AddItemToCart(product)">Add to cart</button>
+
+        </div>
+      </div>
+      </div>
+
+</template>
+
+
+
+<script>
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+export default {
+    data: () => {
+        return {
+            products: [
+        {
+          name: 'Jacket',
+          cost: '559 SEK',
+          images: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPoD9T_CW4YMZ34jCTpDc-d8xF2xyblY99Gg&usqp=CAU", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjWAiMAVp7rROceRF6flIsA5kbO2S0OycFmw&usqp=CAU"
+          ]    
+        }, 
+
+
+        {
+          name: "Dress",
+          cost: "349 SEK",
+          images: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRBxeX9-L4J512hLmqEnqx3l_yLTbykS4H6w&usqp=CAU","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyNdLyFO79LdOaLY_VMm9fE2GX2Twmyuu0Sw&usqp=CAU"
+          ]
+    
+        },
+
+        {
+          name: "Socks",
+          cost: "101 SEK",
+          images: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzuInjFKU0dKkd0W42R6tFp5cBWMhjhd59ZQ&usqp=CAU", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu7npG_vvqUxlMEwYIbLW8M5kvybj6MEFP1g&usqp=CAU"
+          ]
+        },
+        {
+           name: "Jeans",
+           cost: "1000 SEK",
+           images: [
+           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt2fvxT0kNRqKFP9Dx1y24cS8D13HknuSYPw&usqp=CAU", 
+           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStZ2kuKNuAUddM6lO7-JGiPubyO9C5vUDfRQ&usqp=CAU",
+           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuRdLrsj2IFzUta38rNSlro6sB03cl4VWnOQ&usqp=CAU"]
+        }
+      ],
+     
+
+        };
+
+    },
+    methods: {
+        AddItemToCart(product) {
+            this.$emit('AddItemToCart', product)
+        },
+        
+  
+
+  
+    },
+    components: {
+          Carousel,
+          Slide,
+          Pagination,
+          Navigation,
+  
+    }
+
+}
+
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0%;
+}
+* {
+  box-sizing: content-box;
+}
+
+.carousel_slide {
+  background-color: black;
+  color: white;
+  font-size: 20px;
+  border: 2px solid white;
+  border-radius: 5px;
+  height: 200px;
+  width: 50px;
+}
+
+.carousel {
+  position: relative;
+  width: 250px;
+  
+  
+
+}
+
+img {
+  width: 200px;
+  height:269px;
+}
+
+.carousel__prev,
+.carousel__next {
+  background-color: black;
+	box-sizing: content-box;
+}
+
+.carousel__pagination-button {
+  background-color: lightgray;
+}
+
+.carousel__pagination-button--active {
+  background-color: black;
+}
+
+button {
+  margin-bottom: 4vh;
+  background-color: ivory;
+  border-radius: 3%;
+  font-family: verdana;
+}
+
+.ProductsWrapper {
+
+  margin-left: 5vh;
+}
+</style>
+
